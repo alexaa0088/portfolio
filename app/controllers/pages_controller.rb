9d8c50606c19766e89a8e33221a6c8942727de9d
alexa08 <alexaa0088@gmail.com>
2015-09-01 18:27:52 +0000

    Added contact and portfolio action, routes and pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,16 @@
 class PagesController < ApplicationController
     
     def home
-        
+    end
+    
+    def portfolio
     end
     
     def about
-        
     end
+    
+    def contact
+    end
+    
     
 end
